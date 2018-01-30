9f7d02c90d8a08fba9f1b7aba7a6030e6259d97c
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2018-01-30 21:45:22 -0200

    Finalizando alterações: projetos adicionados ao campo "portfolio"!
    Falta organizar alguns detalhes, como alinhamento de alguns elementos textuais, entre outros. Por hora, está ok! =D

---
+++ projetos/projeto-estagio/detalhes/js/detalhes.js
@@ -0,0 +1,87 @@
+    var greenTruck = 'url(\'../../projeto-estagio/assets/truck_green.png\') bottom right 15% no-repeat #46B6AC';
+    var yellowTruck = 'url(\'../../projeto-estagio/assets/truck_yellow.png\') bottom right 15% no-repeat #46B6AC';
+    var redTruck = 'url(\'../../projeto-estagio/assets/truck_red.png\') bottom right 15% no-repeat #46B6AC';
+
+
+    var id = localStorage.getItem('id');
+    var name = localStorage.getItem('name');
+    var fuel = localStorage.getItem('fuel');
+
+    var infoJsonTruck;
+    var date;
+    var fuelValues = [];
+
+    $(document).ready(function(){
+    choosedCard = '<div class="mdl-cell">' +
+                      '<div class="mdl-card mdl-shadow--3dp demonstracao-card">' +
+                        '<h3 class="mdl-card__title-text alignCenter">' + 'ID:&nbsp' + id + '</h4>' +
+                        '<div id="card_to_change" class="mdl-card__title">' +
+                          '<h2 class="mdl-card__title-text">' + name + '</h2>' +
+                        '</div>' +
+                        '<h4 class="mdl-card__title-text">Combustível:&nbsp' + fuel + '%' + '</h4>' +
+                        '<div id="barra" class="progressbar"><div></div></div>' +                                                     
+                      '</div>' +
+                    '</div>' +
+                    '<div class="mdl-tooltip" data-mdl-for="barra">' + fuel + '%' + '</div>' +
+                    '<div class="mdl-cell--6-col"><canvas class="line-chart"></canvas></div>';
+
+    $('#truck_one_card').append(choosedCard);
+
+    var myElement = document.querySelector("#card_to_change");
+    var progressBar = document.querySelector(".progressbar>div");
+
+    if (fuel <= 30)
+    {
+        myElement.style.background = redTruck;
+        progressBar.style.width = fuel + '%'; 
+        progressBar.style.backgroundColor = 'red'; 
+    } 
+    else if (fuel > 30 && fuel <= 60)
+    {
+        myElement.style.background = yellowTruck;
+        progressBar.style.width = fuel + '%';
+        progressBar.style.backgroundColor = 'yellow'; 
+    } 
+    else
+    {
+        myElement.style.background = greenTruck;
+        progressBar.style.width = fuel + '%';
+        progressBar.style.backgroundColor = 'lightgreen'; 
+    }
+    });
+
+    $.getJSON('https://raw.githubusercontent.com/Pedrohf360/html-css-javascript/master/projeto-estagio/detalhes/json/' + id + '.json', function(data){
+
+      setTruckInfo(data);
+
+      setTimeout(createChart(), 1000);
+    });
+
+    function setTruckInfo(data){
+      date = data[0].Date.substring(0, 10);
+
+      for (var i = 0; i < 12*120; i+=120) {
+        fuelValues.push(data[i].Value.toFixed(2));
+      }
+    }
+
+    function createChart()
+    {
+      var ctx = document.getElementsByClassName("line-chart");
+
+      var chartGraph = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: ["00:00", "02:00", "04:00", "6:00", "8:00", "10:00", "12:00", "14:00", "16:00", "18:00", "20:00", "22:00"],
+          datasets: [{
+            label: 'VOLUME COMBUSTÍVEL',
+            data: [fuelValues[0], fuelValues[1], fuelValues[2], fuelValues[2], fuelValues[3], fuelValues[4], fuelValues[5],
+                    fuelValues[6], fuelValues[7], fuelValues[8], fuelValues[9], fuelValues[10], fuelValues[11]],
+            borderWidth: 6,
+            borderColor: 'rgba(77,166,253,0.85)',
+            backgroundColor: 'transparent'
+                  },
+              ]
+        }
+      });
+    }
---
+++ projetos/projeto-estagio/js/main.js
@@ -0,0 +1,76 @@
+var greenTruck = 'url(\'../projeto-estagio/assets/truck_green.png\') bottom right 15% no-repeat #46B6AC';
+var yellowTruck = 'url(\'../projeto-estagio/assets/truck_yellow.png\') bottom right 15% no-repeat #46B6AC';
+var redTruck = 'url(\'../projeto-estagio/assets/truck_red.png\') bottom right 15% no-repeat #46B6AC';
+
+var arr = [];
+
+$(document).ready(function(){
+    $.getJSON('https://raw.githubusercontent.com/Pedrohf360/html-css-javascript/master/projeto-estagio/json/equipamentos.json', function(data){
+    
+        var truck = '';
+
+        $.each(data, function(key, value){
+            var slicedValue = value.Value.toString().substring(0, 2);   
+            var fuelValue = '<strong>' + slicedValue + '%' + '</strong>';
+
+            truck += '<div class="mdl-cell">' +
+                      '<div class="mdl-card mdl-shadow--3dp demonstracao-card">' +
+                        '<h3 class="mdl-card__title-text alignCenter">' + 'ID:&nbsp' + value.ID + '</h4>' +
+                        '<div class="mdl-card__title">' +
+                          '<h2 class="mdl-card__title-text">' + value.Name + '</h2>' +
+                        '</div>' +
+                        '<h4 class="mdl-card__title-text">Combustível:&nbsp' + fuelValue + '</h4>' +
+                        '<div id="progressbar' + key + '" class="progressbar"><div></div></div>' +
+                        '<div class="mdl-card__actions mdl-card--border">' +                    //, + fuelValue + ',' + value.Name + ',' + value.ID +'
+                          '<button class="mdl-button mdl-js-button mdl-button--primary" value="' + value.ID + ';' + value.Name + ';' + slicedValue + '" onClick="reply_click(this.value)" id="' + key + '">' + '<strong>' + 'VER DETALHES' + '</strong>' +'</button>' +
+                        '</div>' +                                                      
+                      '</div>' +
+                    '</div>' +
+                    '<div class="mdl-tooltip" data-mdl-for="progressbar' + key + '">' + value.Value.toFixed(2) + '%' + '</div>';
+
+          arr[key] = slicedValue;
+
+        });
+
+        truck += '<div class="spacer"></div>'
+
+        $('#truck_cards').append(truck);
+
+        var myElements = document.querySelectorAll('.mdl-card__title');
+
+        for (var i = 0; i < myElements.length; i++) {
+            
+            var progressBar = document.querySelectorAll('.progressbar>div');
+
+            if (arr[i] <= 30)
+            {
+                myElements[i].style.background = redTruck;
+                progressBar[i].style.width = arr[i] + '%'; 
+                progressBar[i].style.backgroundColor = 'red'; 
+            } 
+            else if (arr[i] > 30 && arr[i] <= 60)
+            {
+                myElements[i].style.background = yellowTruck;
+                progressBar[i].style.width = arr[i] + '%';
+                progressBar[i].style.backgroundColor = 'yellow'; 
+            } 
+            else 
+            {
+                myElements[i].style.background = greenTruck;
+                progressBar[i].style.width = arr[i] + '%';
+                progressBar[i].style.backgroundColor = 'lightgreen'; 
+            }
+          }
+    });
+});    
+
+function reply_click(choosedInfo)
+{
+    var info = choosedInfo.split(';');
+
+    localStorage.setItem('id', info[0]);
+    localStorage.setItem('name', info[1]);
+    localStorage.setItem('fuel', info[2]);
+
+    window.location = '../projeto-estagio/detalhes/detalhes.html';
+}
